d4e9742f50d54bfd6845ebc611e70a6536c1b160
alfiy <alfi.cakap@gmail.com>
2020-07-07 14:27:19 +0700

    styling header top nav

--- resources/js/components/pages/Header.vue
+++ resources/js/components/pages/Header.vue
@@ -1,8 +1,15 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+    <nav
+        class="navbar navbar-expand-lg navbar-light bg-light border-bottom top-nav"
+    >
         <div class="container">
-            <div class="navbar-brand">
-                <font>Gratis Ebook 9 Cara Cerdas Menggunakan Domain[x]</font>
+            <div class="navbar-brand top-brand">
+                <span class="post-tag">
+                    <i class="fa fa-tag" aria-hidden="true"></i>
+                </span>
+                <small
+                    >Gratis Ebook 9 Cara Cerdas Menggunakan Domain[ x ]</small
+                >
             </div>
             <button
                 class="navbar-toggler"
@@ -16,17 +23,27 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#"
-                            >Home <span class="sr-only">(current)</span></a
-                        >
+                <ul class="navbar-nav ml-auto subnav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-phone" aria-hidden="true"></i>
+                            &nbsp;0274-5305505
+                        </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-comment" aria-hidden="true"></i>
+                            &nbsp;Live Chat
+                        </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="#">
+                            <i
+                                class="fa fa-user rounded-circle"
+                                aria-hidden="true"
+                            ></i>
+                            &nbsp;Member Area
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -39,3 +56,33 @@ export default {
     name: "Header"
 };
 </script>
+
+<style lang="scss" scoped>
+.subnav .nav-item {
+    .nav-link {
+        font-family: "Roboto-Light", sans-serif;
+        color: #676767;
+        .fa-user {
+            background: #4d4d4d;
+            padding: 0.3em 0.4em;
+            color: #ffffff;
+        }
+    }
+}
+
+.post-tag {
+    clip-path: polygon(100% 0, 100% 100%, 51% 73%, 0 100%, 0 0);
+    background: #406df3;
+    color: #ffffff;
+    padding: 2px 7px 7px;
+}
+
+@media (min-width: 992px) {
+    .top-brand {
+        position: absolute;
+        top: 0;
+        color: #676767;
+        padding: 0;
+    }
+}
+</style>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel Vue</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="{{asset("css/app.css")}}">
     </head>
     <body>
