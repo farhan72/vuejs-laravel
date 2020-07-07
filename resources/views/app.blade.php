f8e82e584323015eec063792e0cf93f49971114a
alfiy <alfi.cakap@gmail.com>
2020-07-07 12:30:17 +0700

    install vue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <title>Laravel Vue</title>
+        <link rel="stylesheet" href="{{asset("css/app.css")}}">
     </head>
     <body>
-        <div id="app"></div>
+        <div id="app">
+            <example-component></example-component>
+        </div>
         <script src="{{asset('js/app.js')}}"></script>
     </body>
 </html>
