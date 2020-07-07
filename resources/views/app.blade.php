e922dcbe808a671275f4a24ad4a73b96f8df3655
alfiy <alfi.cakap@gmail.com>
2020-07-07 12:50:38 +0700

    first commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
     </head>
     <body>
         <div id="app">
-            <example-component></example-component>
+            <layout-component></layout-component>
         </div>
         <script src="{{asset('js/app.js')}}"></script>
     </body>
